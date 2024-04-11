e290671a035a8b744c3396054b50fde3fbc3a2de
schluis <luis.scheuch@posteo.de>
2024-04-11 19:32:46 +0200

    update box symbol

--- lab01/monkey_banana/banana_environment.py
+++ lab01/monkey_banana/banana_environment.py
@@ -88,10 +88,10 @@ class MonkeyBananaEnvironmentTask:
         
         if not self.state.is_monkey_up:
             level_0 = [" "] * (self.room_size * 2 - 2)
-            level_0[max(self.state.box_position * 2 - 1, 0)] = '▅▅'
+            level_0[max(self.state.box_position * 2 - 1, 0)] = '██'
         else:
             level_0 = [" "] * (self.room_size * 2 - 1)
-            level_0[self.state.box_position * 2] = '▅▅'
+            level_0[self.state.box_position * 2] = '██'
         
            
         if not(self.state.is_monkey_up):
